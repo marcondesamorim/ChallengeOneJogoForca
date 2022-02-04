a83fb0740d45a55d30770167498e58eee2f53e5f
Marcondes Amorim <marcondesjra@gmail.com>
2022-02-04 11:03:08 -0300

    First Commit

---
+++ jsp/forca.js
@@ -0,0 +1,7 @@
+let btnIniciar =document.getElementById("iniciar-jogo");
+
+btnIniciar.addEventListener("click",iniciarJogo)
+
+function iniciarJogo(){
+    window.location.href = "./pages/jogo.html";
+}
